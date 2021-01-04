87d41ff3aa6ece4980a15219bb32a149c5dc3db5
Nathan <kamenchunathan@gmail.com>
2021-01-04 23:02:56 +0300

    feat: Add ability for Clock to schedule functions once on the next clock iteration

--- src/main.py
+++ src/main.py
@@ -6,46 +6,79 @@ import traceback
 import mechanicalsoup
 import requests
 from mechanicalsoup import LinkNotFoundError
-from message_service import Messenger
+from message_service import Messenger, TwilioLiveSMSBackend
+from logger import log_updated_message
 
 
 class EventLoop:
-    def __init__(self, global_exception_handler=None):
-        self.delta_time = 20
-        self.__scheduled_functions = []
-        self.__exception_handler = global_exception_handler
-        self.__set_up_functions = []
-        self._should_break = False
+    delta_time = 20
 
-    def schedule_set_up(self, func):
-        self.__set_up_functions.append(func)
+    __scheduled_functions = []
+    __exception_handler = None
+    __set_up_functions = []
+    __single_iter_scheduled_functions = []
 
-    def schedule_repeating(self, function):
-        self.__scheduled_functions.append(function)
+    _should_break = False
 
-    def unschedule(self, function):
-        if function in self.__scheduled_functions:
-            self.__scheduled_functions.remove(function)
+    @classmethod
+    def schedule_set_up(cls, func):
+        cls.__set_up_functions.append(func)
+
+    @classmethod
+    def schedule_repeating(cls, func):
+        cls.__scheduled_functions.append(func)
+
+    @classmethod
+    def schedule_once(cls, func, *args, **kwargs):
+        """
+        Schedules a func for execution in the next iteration of the loop if 
+        the time elapsed exceeds the delay set
+        
+        :param func: func to call
+        :param delay: time in seconds that should pass before the func is called again
+        :param *args, **kwargs All other arguments are passed to the function to be called
+        :return: None
+        """
+        cls.__single_iter_scheduled_functions.append({
+            'time_scheduled': datetime.datetime.now(),
+            'delay': kwargs.pop('delay', 0),
+            'func': func,
+            'args': args,
+            'kwargs': kwargs
+        })
+
+    @classmethod
+    def unschedule(cls, func):
+        if func in cls.__scheduled_functions:
+            cls.__scheduled_functions.remove(func)
             return True
         return False
 
-    def run_set_up_functions(self):
+    @classmethod
+    def run_set_up_functions(cls):
         """
         Runs all the functions defined as set_up functions
 
         It is called before all scheduled functions
         """
-        while len(self.__set_up_functions):
-            self.__set_up_functions.pop()()
+        while len(cls.__set_up_functions):
+            cls.__set_up_functions.pop()()
 
-    def should_break(self):
+    @classmethod
+    def should_break(cls):
         """ Determines whether the main loop should continue executing"""
-        return self._should_break
+        return cls._should_break
 
-    def schedule_bread(self):
-        self._should_break = True
+    @classmethod
+    def schedule_bread(cls):
+        cls._should_break = True
 
-    def run(self):
+    @classmethod
+    def set_global_exception_handler(cls, func):
+        cls.__exception_handler = func
+
+    @classmethod
+    def run(cls):
         """
         The main loop
 
@@ -53,23 +86,36 @@ class EventLoop:
         seconds specified in delta_time attribute
 
         If an exception occurs during the execution of the loop the exception handler
-        function is called to handle it if defined. Otherwise No action is taken
+        func is called to handle it if defined. Otherwise No action is taken
 
-        Polls the should break function to determine whether the loop should close and execution stop
+        Polls the should break func to determine whether the loop should close and execution stop
 
         :return:
         """
-        self.run_set_up_functions()
+        cls.run_set_up_functions()
         while True:
             try:
-                for func in self.__scheduled_functions:
+                # call all functions that have been scheduled once if appropriate
+                for func_details in cls.__single_iter_scheduled_functions:
+                    elapsed_time = datetime.timedelta(seconds=func_details['delay'])
+                    if func_details['time_scheduled'] + elapsed_time < datetime.datetime.now():
+                        try:
+                            args = func_details['args'] if func_details['args'] else ()
+                            kwargs = func_details['kwargs'] if func_details['kwargs'] else {}
+                            func_details['func'](*args, **kwargs)
+                        finally:
+                            cls.__single_iter_scheduled_functions.remove(func_details)
+
+                # call all functions that have been scheduled to repeat
+                for func in cls.__scheduled_functions:
                     func()
-                time.sleep(self.delta_time)
-                if self.should_break():
+
+                time.sleep(cls.delta_time)
+                if cls.should_break():
                     break
             except Exception as e:
-                if self.__exception_handler:
-                    self.__exception_handler(e)
+                if cls.__exception_handler:
+                    cls.__exception_handler(e)
                 elif not os.environ.get('DEBUG'):
                     raise
 
@@ -86,7 +132,7 @@ def set_up():
 
 
 def check_room_booking_open():
-    """Main function that is run to check whether the room booking application link has been opened"""
+    """Main func that is run to check whether the room booking application link has been opened"""
     browser = mechanicalsoup.StatefulBrowser()
 
     # log in to student portal
@@ -106,9 +152,11 @@ def check_room_booking_open():
         browser.select_form()
     except LinkNotFoundError:
         pass
-    # else:
-        sms_sender = Messenger()
-        sms_sender.send_message('+254705493474', 'Hello World')
+    else:
+        sms_sender = Messenger(backend=TwilioLiveSMSBackend())
+        message = sms_sender.send_message('+254705493474', 'Hello World')
+
+        EventLoop.schedule_once(log_updated_message, message)
 
 
 def exception_handler(e):
@@ -120,7 +168,7 @@ def exception_handler(e):
 
 
 if __name__ == '__main__':
-    loop = EventLoop(exception_handler)
-    loop.schedule_set_up(set_up)
-    loop.schedule_repeating(check_room_booking_open)
-    loop.run()
+    EventLoop.set_global_exception_handler(exception_handler)
+    EventLoop.schedule_set_up(set_up)
+    EventLoop.schedule_repeating(check_room_booking_open)
+    EventLoop.run()
