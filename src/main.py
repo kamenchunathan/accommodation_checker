9191fb5d917c603a7b0e213adfff18ff3865dfa2
Nathan <kamenchunathan@gmail.com>
2021-01-04 13:54:46 +0300

    feat: Add working sms backend

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
-import sys
+import os
 
-print(sys.version_info)
+from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+to_phone_number = os.environ['TO_PHONE_NUMBER']
+from_phone_number = os.environ['FROM_PHONE_NUMBER']
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    body="IT WOOOOOOORKSS!!!!!",
+    from_=from_phone_number,
+    to='+254705493474'
+)
+
+print(message.sid)
