9d55531da4e54b9365edc8d46aca22d794b3f088
Nathan <kamenchunathan@gmail.com>
2021-01-04 16:13:29 +0300

    refactor: Extract message sending to backend class

--- src/message_backend.py
+++ src/message_backend.py
@@ -2,19 +2,26 @@ import os
 
 from twilio.rest import Client
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-to_phone_number = os.environ['TO_PHONE_NUMBER']
-from_phone_number = os.environ['FROM_PHONE_NUMBER']
-
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    body="IT WOOOOOOORKSS!!!!!",
-    from_=from_phone_number,
-    to='+254705493474'
-)
+from .logger import log_message
 
 
-messages = TwilioLiveSMSBackend().send_message('wow')
-print(messages)
+class TwilioLiveSMSBackend:
+    """
+    An SMS backend to handle sending messages from a preconfigured twilio number to a recipient specified bu calling
+    the send message method
+    """
+
+    def __init__(self):
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        self.client = Client(account_sid, auth_token)
+        self.sender_phone_number = from_phone_number = os.environ['FROM_PHONE_NUMBER']
+
+    @log_message
+    def send_message(self, message_body, recipient_phone_no):
+        message = self.client.messages.create(
+            body=message_body,
+            from_=self.sender_phone_number,
+            to=recipient_phone_no
+        )
+        return message
